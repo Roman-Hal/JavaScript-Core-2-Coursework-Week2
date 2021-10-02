586d78e106aba4c7014d3a0bf6da4437eb549b87
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-10-02 22:32:21 +0100

    exercise-B-shopping-list-finished

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+  let array = Array.from(arrayOfPeople);
+  let content = document.getElementById("content");
+  var ul = document.createElement("ul");
+  for (let i = 0; i<array.length; i++) {
+    var li = document.createElement("li");
+    li.innerHTML = array[i];
+    ul.appendChild(li); 
+  };
+  content.appendChild(ul);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
