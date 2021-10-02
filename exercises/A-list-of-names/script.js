51e9918b93da51c5d58939a8f61f6afad79654cc
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-10-02 22:16:14 +0100

    exercises-A-list-of-names-finished

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,16 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  var head1 = document.createElement("h1");
+  var head2 = document.createElement("h2");
+  head1.innerHTML = `${arrayOfPeople.name}`;
+  head2.innerHTML = `${arrayOfPeople.job}`;
+  content.appendChild(head1).after(head2);
+  }
+
+function showRes() {
+  people.forEach((arrayOfPeople) => {listOfNames(arrayOfPeople);});
 }
+
 
 let people = [
   { name: "Chris", job: "Teacher" },
@@ -8,4 +18,5 @@ let people = [
   { name: "Boris", job: "Prime Minister" },
 ];
 
-listOfNames(people);
+showRes();
+//listOfNames(people);
