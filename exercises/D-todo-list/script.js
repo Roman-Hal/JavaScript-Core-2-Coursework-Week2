b9e5af5289ea5ae76d3c72454289acee61fcbe2c
Roman-Hal <85693082+Roman-Hal@users.noreply.github.com>
2021-10-23 10:22:06 +0100

    D-todo-list-finished

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,31 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.getElementById("content");
+  let ul = document.createElement("ul");
+  for (let i = 0; i < todos.length; i++) {
+    let list = document.createElement('li');
+    list.innerHTML = `${todos[i].todo}`;
+    ul.appendChild(list);
+    list.style.listStyle = "none";
+    if(i%2===0) {
+      list.style.backgroundColor = "lightgrey";
+    }else {
+      list.style.backgroundColor = "grey";
+    }
+    list.style.cursor = "pointer";
+  }
+  
+  content.appendChild(ul);
+
+  function line(ev) {
+    if(ev.target.style.getPropertyValue('text-decoration').startsWith('line-through',0)) 
+    {
+      ev.target.style.textDecoration = 'none';
+    } else {
+      ev.target.style.textDecoration = 'line-through';
+    }
+  }
+  ul.addEventListener('click', line);
 }
 
 const todos = [
